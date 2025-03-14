515d382379ccbf319d023268206692fec8624be2
Mark Griffin <mark.griffin@linux.com>
2025-03-14 14:18:45 +0100

    Generated

--- valheim-easy.bat
+++ valheim-easy.bat
@@ -2,6 +2,6 @@
 
 echo Downloading and setting addon config...
 
-curl https://raw.githubusercontent.com/nerdgeneration/NeverPortal-Configs/main/valheim-easy/Azumatt.HelheimHarmonizer.cfg -o %appdata%\r2modmanPlus-local\Valheim\profiles\Default\BepInEx\config\Azumatt.HelheimHarmonizer.cfg
 curl https://raw.githubusercontent.com/nerdgeneration/NeverPortal-Configs/main/valheim-easy/advize.PlantEverything.cfg -o %appdata%\r2modmanPlus-local\Valheim\profiles\Default\BepInEx\config\advize.PlantEverything.cfg
+curl https://raw.githubusercontent.com/nerdgeneration/NeverPortal-Configs/main/valheim-easy/Azumatt.HelheimHarmonizer.cfg -o %appdata%\r2modmanPlus-local\Valheim\profiles\Default\BepInEx\config\Azumatt.HelheimHarmonizer.cfg
 curl https://raw.githubusercontent.com/nerdgeneration/NeverPortal-Configs/main/valheim-easy/valheim_plus.cfg -o %appdata%\r2modmanPlus-local\Valheim\profiles\Default\BepInEx\config\valheim_plus.cfg
