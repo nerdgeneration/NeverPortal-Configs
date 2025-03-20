9a3db9c6546c89b937d6a03d258f9ce9c884b933
Mark Griffin <mark.griffin@linux.com>
2025-03-20 14:15:53 +0100

    Updated scripts

--- valheim-easy.bat
+++ valheim-easy.bat
@@ -2,6 +2,19 @@
 
 echo Downloading and setting addon config...
 
-curl https://raw.githubusercontent.com/nerdgeneration/NeverPortal-Configs/main/valheim-easy/advize.PlantEverything.cfg -o %appdata%\r2modmanPlus-local\Valheim\profiles\Default\BepInEx\config\advize.PlantEverything.cfg
 curl https://raw.githubusercontent.com/nerdgeneration/NeverPortal-Configs/main/valheim-easy/Azumatt.HelheimHarmonizer.cfg -o %appdata%\r2modmanPlus-local\Valheim\profiles\Default\BepInEx\config\Azumatt.HelheimHarmonizer.cfg
+curl https://raw.githubusercontent.com/nerdgeneration/NeverPortal-Configs/main/valheim-easy/HookGenPatcher.cfg -o %appdata%\r2modmanPlus-local\Valheim\profiles\Default\BepInEx\config\HookGenPatcher.cfg
+curl https://raw.githubusercontent.com/nerdgeneration/NeverPortal-Configs/main/valheim-easy/Jotunn.dll -o %appdata%\r2modmanPlus-local\Valheim\profiles\Default\BepInEx\config\Jotunn.dll
+curl https://raw.githubusercontent.com/nerdgeneration/NeverPortal-Configs/main/valheim-easy/MMHOOK_assembly_googleanalytics.dll -o %appdata%\r2modmanPlus-local\Valheim\profiles\Default\BepInEx\config\MMHOOK_assembly_googleanalytics.dll
+curl https://raw.githubusercontent.com/nerdgeneration/NeverPortal-Configs/main/valheim-easy/MMHOOK_assembly_guiutils.dll -o %appdata%\r2modmanPlus-local\Valheim\profiles\Default\BepInEx\config\MMHOOK_assembly_guiutils.dll
+curl https://raw.githubusercontent.com/nerdgeneration/NeverPortal-Configs/main/valheim-easy/MMHOOK_assembly_lux.dll -o %appdata%\r2modmanPlus-local\Valheim\profiles\Default\BepInEx\config\MMHOOK_assembly_lux.dll
+curl https://raw.githubusercontent.com/nerdgeneration/NeverPortal-Configs/main/valheim-easy/MMHOOK_assembly_postprocessing.dll -o %appdata%\r2modmanPlus-local\Valheim\profiles\Default\BepInEx\config\MMHOOK_assembly_postprocessing.dll
+curl https://raw.githubusercontent.com/nerdgeneration/NeverPortal-Configs/main/valheim-easy/MMHOOK_assembly_simplemeshcombine.dll -o %appdata%\r2modmanPlus-local\Valheim\profiles\Default\BepInEx\config\MMHOOK_assembly_simplemeshcombine.dll
+curl https://raw.githubusercontent.com/nerdgeneration/NeverPortal-Configs/main/valheim-easy/MMHOOK_assembly_sunshafts.dll -o %appdata%\r2modmanPlus-local\Valheim\profiles\Default\BepInEx\config\MMHOOK_assembly_sunshafts.dll
+curl https://raw.githubusercontent.com/nerdgeneration/NeverPortal-Configs/main/valheim-easy/MMHOOK_assembly_utils.dll -o %appdata%\r2modmanPlus-local\Valheim\profiles\Default\BepInEx\config\MMHOOK_assembly_utils.dll
+curl https://raw.githubusercontent.com/nerdgeneration/NeverPortal-Configs/main/valheim-easy/MMHOOK_assembly_valheim.dll -o %appdata%\r2modmanPlus-local\Valheim\profiles\Default\BepInEx\config\MMHOOK_assembly_valheim.dll
+curl https://raw.githubusercontent.com/nerdgeneration/NeverPortal-Configs/main/valheim-easy/PlanBuild.dll -o %appdata%\r2modmanPlus-local\Valheim\profiles\Default\BepInEx\config\PlanBuild.dll
+curl https://raw.githubusercontent.com/nerdgeneration/NeverPortal-Configs/main/valheim-easy/advize.PlantEverything.cfg -o %appdata%\r2modmanPlus-local\Valheim\profiles\Default\BepInEx\config\advize.PlantEverything.cfg
+curl https://raw.githubusercontent.com/nerdgeneration/NeverPortal-Configs/main/valheim-easy/marcopogo.PlanBuild.cfg -o %appdata%\r2modmanPlus-local\Valheim\profiles\Default\BepInEx\config\marcopogo.PlanBuild.cfg
+curl https://raw.githubusercontent.com/nerdgeneration/NeverPortal-Configs/main/valheim-easy/update -o %appdata%\r2modmanPlus-local\Valheim\profiles\Default\BepInEx\config\update
 curl https://raw.githubusercontent.com/nerdgeneration/NeverPortal-Configs/main/valheim-easy/valheim_plus.cfg -o %appdata%\r2modmanPlus-local\Valheim\profiles\Default\BepInEx\config\valheim_plus.cfg
